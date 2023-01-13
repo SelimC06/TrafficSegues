ea3825c193bc38e339b2c3462b7e85fe16fb02ea
SelimC06 <scoskunuzer@gmail.com>
2023-01-13 09:14:20 -0600

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Coskunuzer, Selim on 1/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
